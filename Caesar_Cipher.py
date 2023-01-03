e75a8007f881838c3393e871a0e8648e051d7f50
Braden Piper <bgpiper@gmail.com>
2023-01-03 14:24:44 -0600

    Remove unnecessary PlainTextMessage Class

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -166,68 +166,7 @@ class Message(object):
                 shiftedString += letter
         
         return shiftedString
-        
-        
-
-class PlaintextMessage(Message):
-    def __init__(self, text, shift):
-        '''
-        Initializes a PlaintextMessage object        
-        
-        text (string): the message's text
-        shift (integer): the shift associated with this message
-
-        A PlaintextMessage object inherits from Message and has five attributes:
-            self.message_text (string, determined by input text)
-            self.valid_words (list, determined using helper function load_words)
-            self.shift (integer, determined by input shift)
-            self.encrypting_dict (dictionary, built using shift)
-            self.message_text_encrypted (string, created using shift)
-        '''
-        self.message_text = text
-        self.shift = shift
-        self.encrypting_dict = Message.build_shift_dict(self,shift)
-        self.message_text_encrypted = Message.apply_shift(self,shift)
-
-    def get_shift(self):
-        '''
-        Used to safely access self.shift outside of the class
-        
-        Returns: self.shift
-        '''
-        return self.shift
-
-    def get_encrypting_dict(self):
-        '''
-        Used to safely access a copy self.encrypting_dict outside of the class
-        
-        Returns: a COPY of self.encrypting_dict
-        '''
-        return self.encrypting_dict.copy()
-
-    def get_message_text_encrypted(self):
-        '''
-        Used to safely access self.message_text_encrypted outside of the class
-        
-        Returns: self.message_text_encrypted
-        '''
-        return self.message_text_encrypted[:]
-
-    def change_shift(self, shift):
-        '''
-        Changes self.shift of the PlaintextMessage and updates other 
-        attributes determined by shift (ie. self.encrypting_dict and 
-        message_text_encrypted).
-        
-        shift (integer): the new shift that should be associated with this message.
-        0 <= shift < 26
-
-        Returns: nothing
-        '''
-        self.shift = shift
-        self.encrypting_dict = Message.build_shift_dict(self,shift)
-        self.message_text_encrypted = Message.apply_shift(self,shift)
-
+    
 
 class CiphertextMessage(Message):
     def __init__(self, text):
@@ -324,7 +263,7 @@ print('Welcome agent. I am Caesar Cipher, here to encode and decode your message
 userChoice = whichTask()
 if userChoice == 1:
     encryptedMessage = encrypter()
-    print('Your encoded message is:', encryptedMessage)
+    print('Your encrypted message is:', encryptedMessage)
 elif userChoice == 2:
     decryptedMessage = decrypter()
-    print('Your decoded message is:', decryptedMessage)
+    print('Your decrypted message is:', decryptedMessage)
